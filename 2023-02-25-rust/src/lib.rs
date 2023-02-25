1e018c11f0f419f14ed64f7754300f8ab797ab74
Belén Albeza <belen@hey.com>
2023-02-25 18:11:30 +0100

    implement reading unit list

--- 2023-02-25-rust/src/lib.rs
+++ 2023-02-25-rust/src/lib.rs
@@ -1,17 +1,21 @@
 use std::io;
 
-pub fn run<R>(reader: R) -> Result<String, std::num::ParseFloatError>
+pub fn run<R>(reader: &mut R) -> Result<String, String>
 where
     R: io::BufRead,
 {
     let raw_prices = read_line(reader);
-    let prices = parse_prices(&raw_prices)?;
+    let prices = parse_prices(&raw_prices).map_err(|_| "Error reading prices".to_string())?;
+
+    let raw_units = read_line(reader);
+    let _ = parse_units(&raw_units).map_err(|_| "Error reading units".to_string())?;
+
     let net_price = net_price_for_order(&prices);
 
     Ok(format!("{}", net_price))
 }
 
-fn read_line<R>(mut reader: R) -> String
+fn read_line<R>(reader: &mut R) -> String
 where
     R: io::BufRead,
 {
@@ -22,6 +26,10 @@ where
 
 fn parse_prices(text: &str) -> Result<Vec<f64>, std::num::ParseFloatError> {
     text.split(" ").map(|x| x.parse::<f64>()).collect()
+}
+
+fn parse_units(text: &str) -> Result<Vec<u64>, std::num::ParseIntError> {
+    text.split(" ").map(|x| x.parse::<u64>()).collect()
 }
 
 fn net_price_for_order(prices: &[f64]) -> f64 {
--- 2023-02-25-rust/tests/calculator.rs
+++ 2023-02-25-rust/tests/calculator.rs
@@ -2,21 +2,28 @@ use kata_carpaccio::run;
 
 #[test]
 fn test_run_returns_ok_when_no_issues() {
-    let input = b"0";
-    let result = run(&input[..]);
+    let input = b"0\n1";
+    let result = run(&mut &input[..]);
     assert_eq!(result, Ok("0".to_string()));
 }
 
 #[test]
 fn test_run_returns_error_when_invalid_prices() {
     let input = b"Waka";
-    let result = run(&input[..]);
+    let result = run(&mut &input[..]);
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_run_returns_error_when_invalid_units() {
+    let input = b"10\nWaka";
+    let result = run(&mut &input[..]);
     assert!(result.is_err());
 }
 
 #[test]
 fn test_run_returns_the_net_price_for_the_order() {
-    let input = b"9 20";
-    let result = run(&input[..]);
+    let input = b"9 20\n1 1";
+    let result = run(&mut &input[..]);
     assert_eq!(result, Ok("29".to_string()));
 }
