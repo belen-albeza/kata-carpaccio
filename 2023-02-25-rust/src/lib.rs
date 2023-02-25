fde9da8f8e26bf9a5f646a32d0e4a30cce3bec71
Belén Albeza <belen@hey.com>
2023-02-25 17:57:24 +0100

    implement parsing prices

--- 2023-02-25-rust/src/lib.rs
+++ 2023-02-25-rust/src/lib.rs
@@ -1,6 +1,16 @@
 use std::io;
 
-pub fn read_line<R>(mut reader: R) -> String
+pub fn run<R>(reader: R) -> Result<String, std::num::ParseFloatError>
+where
+    R: io::BufRead,
+{
+    let raw_prices = read_line(reader);
+    let prices = parse_prices(&raw_prices)?;
+
+    Ok("0".to_string())
+}
+
+fn read_line<R>(mut reader: R) -> String
 where
     R: io::BufRead,
 {
@@ -9,11 +19,6 @@ where
     buffer.trim_end().to_string()
 }
 
-pub fn run<R>(reader: R) -> Result<String, String>
-where
-    R: io::BufRead,
-{
-    let input = read_line(reader);
-
-    Ok(input)
+fn parse_prices(text: &str) -> Result<Vec<f64>, std::num::ParseFloatError> {
+    text.split(" ").map(|x| x.parse::<f64>()).collect()
 }
--- 2023-02-25-rust/tests/calculator.rs
+++ 2023-02-25-rust/tests/calculator.rs
@@ -1,8 +1,15 @@
 use kata_carpaccio::run;
 
 #[test]
-fn test_runs_returns_ok_when_no_issues() {
+fn test_run_returns_ok_when_no_issues() {
+    let input = b"22 19.95";
+    let result = run(&input[..]);
+    assert_eq!(result, Ok("0".to_string()));
+}
+
+#[test]
+fn test_run_returns_error_when_invalid_prices() {
     let input = b"Waka";
     let result = run(&input[..]);
-    assert_eq!(result, Ok("Waka".to_string()));
+    assert!(result.is_err());
 }
